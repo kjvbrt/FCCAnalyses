53652421f171550f23b088ab3a5ef3630eea0ec2
github-action-benchmark <github@users.noreply.github.com>
2024-07-22 13:46:52 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 1ce364cb240e52dfd07094cdef5beb9177e2ac1e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721656011227,
+  "lastUpdate": 1721656012557,
   "repoUrl": "https://github.com/kjvbrt/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -2771,6 +2771,55 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1ce364cb240e52dfd07094cdef5beb9177e2ac1e",
+          "message": "Adding manual pages for final and pin sub-commands (#393)\n\n* Adding manual pages for final and pin sub-commands\r\n\r\n* Using clang format from the stack\r\n\r\n* Adding safe directory\r\n\r\n* Using stable stack",
+          "timestamp": "2024-07-18T16:07:59Z",
+          "tree_id": "9317ad9021c3f07ad708e98db7c70b3cd3c22aad",
+          "url": "https://github.com/kjvbrt/FCCAnalyses/commit/1ce364cb240e52dfd07094cdef5beb9177e2ac1e"
+        },
+        "date": 1721656012224,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 7.988013135272644,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 9.172914979583542,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 7.116367694561711,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 9.225042613792573,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.22866273224817,
+            "unit": "Evt/s"
           }
         ]
       }
