303078b0476d7c64f0d29553136f1404ac6cbf3c
github-action-benchmark <github@users.noreply.github.com>
2025-08-14 13:22:08 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 705aca3527b7ecbb7c1868d835a80cd8d556e52d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755177726603,
+  "lastUpdate": 1755177728434,
   "repoUrl": "https://github.com/kjvbrt/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -3505,6 +3505,65 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/data_source/analysis_stage1.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "22276694+jmcarcell@users.noreply.github.com",
+            "name": "Juan Miguel Carceller",
+            "username": "jmcarcell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "705aca3527b7ecbb7c1868d835a80cd8d556e52d",
+          "message": "Fix errors with GCC 14 and GCC 13 (#458)\n\nThe issue is that multiple candidates exist:\n```\nedm4hep::CovMatrix3f::operator=(const std::array<float, 6>&)\nedm4hep::CovMatrix3f& edm4hep::CovMatrix3f::operator=(const edm4hep::CovMatrix3f&)\nedm4hep::CovMatrix3f& edm4hep::CovMatrix3f::operator=(edm4hep::CovMatrix3f&&)\n```\nDoes not happen with GCC 15 and Clang 20\n\nCo-authored-by: Juan Miguel Carceller <jmcarcell@users.noreply.github.com>",
+          "timestamp": "2025-08-14T14:55:30+02:00",
+          "tree_id": "faad8a5efb3353d4e18764a27fb77d73571a9e79",
+          "url": "https://github.com/kjvbrt/FCCAnalyses/commit/705aca3527b7ecbb7c1868d835a80cd8d556e52d"
+        },
+        "date": 1755177727736,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 2.9976132609029813,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 9.524736473004552,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 5.723577832205595,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 9.859751324252692,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.746729742483085,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/data_source/stages_source.py",
+            "value": 5.480733273665532,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/data_source/analysis_stage1.py",
+            "value": 5.360624235105613,
+            "unit": "Evt/s"
           }
         ]
       }
