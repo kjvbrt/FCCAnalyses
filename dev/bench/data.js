bfd3c439a181ba429b0ebe4b26b36a5ca8f03789
github-action-benchmark <github@users.noreply.github.com>
2025-11-10 15:20:35 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for f5600716e4cd7094d7a446d06b4fcd4a3969f9fc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762788034206,
+  "lastUpdate": 1762788035844,
   "repoUrl": "https://github.com/kjvbrt/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -3901,6 +3901,65 @@ window.BENCHMARK_DATA = {
             "range": "10",
             "unit": "Seconds",
             "extra": "Analysis path: examples/data_source/analysis_stage1.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f5600716e4cd7094d7a446d06b4fcd4a3969f9fc",
+          "message": "Small improvements to ReconstructedParticle analyzers (#478)\n\n* Small improvements to ReconstructedParticle analyzers\n\n* Formatting\n\n* More formatting",
+          "timestamp": "2025-11-10T16:06:56+01:00",
+          "tree_id": "93dad6a7984dfa8e8303673aa174310e9d18d193",
+          "url": "https://github.com/kjvbrt/FCCAnalyses/commit/f5600716e4cd7094d7a446d06b4fcd4a3969f9fc"
+        },
+        "date": 1762788035184,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 8.36488101500095,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.334529405949496,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 4.822783486790372,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.904679407432079,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.5500231123932906,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/data_source/stages_source.py",
+            "value": 4.562237257321716,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/data_source/analysis_stage1.py",
+            "value": 4.418546703313576,
+            "unit": "Evt/s"
           }
         ]
       }
