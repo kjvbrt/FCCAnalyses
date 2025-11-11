7a606896bd244274974f9cb76c1f9b338681f7c1
github-action-benchmark <github@users.noreply.github.com>
2025-11-11 09:19:55 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 2ee0a698444686998e639fe4a039748835446e28

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762852793652,
+  "lastUpdate": 1762852795453,
   "repoUrl": "https://github.com/kjvbrt/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -4297,6 +4297,65 @@ window.BENCHMARK_DATA = {
             "range": "10",
             "unit": "Seconds",
             "extra": "Analysis path: examples/data_source/analysis_stage1.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2ee0a698444686998e639fe4a039748835446e28",
+          "message": "Plotting: allowing log scaling for the x-axis (#453)\n\n* Plotting: allowing log scaling for the x-axis\n\n* Adding man pages and checks for the input values\n\n* Kebab correction",
+          "timestamp": "2025-11-10T17:08:51Z",
+          "tree_id": "566d042e1fda517bb00f8261a203a800ca38b4ed",
+          "url": "https://github.com/kjvbrt/FCCAnalyses/commit/2ee0a698444686998e639fe4a039748835446e28"
+        },
+        "date": 1762852794732,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 8.719363812805792,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 8.903711954674433,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 0.667103386782038,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 9.48132116183293,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 2.6786744330077465,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/data_source/stages_source.py",
+            "value": 5.161905510870076,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/data_source/analysis_stage1.py",
+            "value": 4.974280186420706,
+            "unit": "Evt/s"
           }
         ]
       }
