453bcd809e4677723ac87c2c23ea42e60adb9733
github-action-benchmark <github@users.noreply.github.com>
2026-07-20 13:56:48 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 82e36982ce06ef03a2d53294f5e76e86c862a695

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784555806764,
+  "lastUpdate": 1784555808661,
   "repoUrl": "https://github.com/kjvbrt/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -379,6 +379,76 @@ window.BENCHMARK_DATA = {
             "value": 5.296111345291138,
             "range": "10",
             "unit": "Seconds",
+            "extra": "Analysis path: /Package/examples/data_source/analysis_stage1.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "82e36982ce06ef03a2d53294f5e76e86c862a695",
+          "message": "Update Zenodo metadata: Contributors, Description, and Community (#517)\n\n* Update Zenodo metadata: add contributors, description, and community identifier\n\n* Sort creators alphabetically in .zenodo.json\n\n* Update Zenodo metadata: add Gerardo Ganis (principal author in man pages) and re-sort alphabetically\n\n* Add ORCID for Juraj Smieško",
+          "timestamp": "2026-07-20T15:38:03+02:00",
+          "tree_id": "27bca25cfe48074e1ee7a23f13c93fa53dbd4799",
+          "url": "https://github.com/kjvbrt/FCCAnalyses/commit/82e36982ce06ef03a2d53294f5e76e86c862a695"
+        },
+        "date": 1784555808237,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: /Package/examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 30.34490482381733,
+            "range": "1000",
+            "unit": "Evt/s",
+            "extra": "Analysis path: /Package/examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/ee/analysis_stage1.py",
+            "value": 13.369482933995466,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 11.344013459072364,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 14.076962850587126,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 4.344219720963461,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/weaver/stage1.py",
+            "value": 5.068855483301066,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: /Package/examples/data_source/stages_source.py",
+            "value": 18.820659296359388,
+            "range": "1000",
+            "unit": "Evt/s",
+            "extra": "Analysis path: /Package/examples/data_source/stages_source.py"
+          },
+          {
+            "name": "nightly | Events processed per second: /Package/examples/data_source/analysis_stage1.py",
+            "value": 18.88177824828243,
+            "range": "1000",
+            "unit": "Evt/s",
             "extra": "Analysis path: /Package/examples/data_source/analysis_stage1.py"
           }
         ]
